69589f4055616d60c76e64768ee24e7d5a5904ad
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-08-12 13:21:15 +1200

    do day 02 part 2

--- src/day02.hs
+++ src/day02.hs
@@ -18,14 +18,19 @@ wrap :: Box -> Int
 wrap (Box l w h) = 2* (sum xs) + minimum xs
     where xs = [l*w, l*h, w*h  ]
 
+ribbon (Box l w h) = (foldl1 (*) xs  ) + 2 *(  sum xs - maximum xs   )
+    where xs = [l,w,h  ]
+
 part1 :: [String] -> Int
 part1 = sum . map (wrap . asBox )
 
+part2 :: [String] -> Int
+part2 = sum . map (ribbon . asBox )
 
 main :: IO ()
 main = do 
           file <- readFile ("../data/02.txt")
           let parts = lines file
           putStrLn ( "p1: " ++ (show $ part1 parts ) )
-          --putStrLn ( "p2: " ++ (show $ part2 parts ) )
+          putStrLn ( "p2: " ++ (show $ part2 parts ) )
 
