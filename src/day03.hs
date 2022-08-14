240bfb813d7c46c935d8f77334fae8d351e8ee1c
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-08-14 12:20:31 +1200

    Do day 03 part 2 On my input, part2 had a lower result than part1. How disappointing.

--- src/day03.hs
+++ src/day03.hs
@@ -12,15 +12,29 @@ conv '<' = (-1) *  ( conv '>' )
 conv _ = error "invalid input"
 
 part1 :: [Char] -> Int
-part1 = length . nub . ( scanl (+) 0 ) . map conv
+part1 = uniques . seen . map conv
 
 
+keepEveryN :: [a] -> Int -> Int -> [a]
+keepEveryN [] _ _ = []
+keepEveryN (x:xs) 0 k = x:(keepEveryN xs (k-1) k)
+keepEveryN (x:xs) n k = keepEveryN xs (n-1) k
 
+seen :: [Complex Float] -> [Complex Float]
+seen = ( scanl (+) 0 )
+
+uniques :: Eq a => [a] -> Int
+uniques = length . nub
+
+
+part2 :: [Char] -> Int
+part2 xs = uniques $ (seen (keepEveryN ys 0 2) ) ++ (seen (keepEveryN ys 1 2) )
+    where ys = map conv xs
 
 main :: IO ()
 main = do 
           file <- readFile ("../data/03.txt")
           putStrLn ( "p1: " ++ (show $ part1 file ) )
-          --putStrLn ( "p2: " ++ (show $ part2 file ) )
+          putStrLn ( "p2: " ++ (show $ part2 file ) )
 
 
