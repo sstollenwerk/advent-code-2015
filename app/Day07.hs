9ebb75187f9cb3b642ef00f2c63a041a388364d5
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-08-22 19:15:31 +1200

    Do day07 part 2

--- app/Day07.hs
+++ app/Day07.hs
@@ -61,28 +61,6 @@ makeValue s
     | otherwise = Right s
 
 
---switch1 :: CircuitState -> Row -> CircuitState
---switch1 s (k, (D1 v)) = Map.insert k (getVal s v) s
---switch1 s (k, (D2 (f, v))) = Map.insert k (f (getVal s v)) s
---switch1 s (k, (D3 (f, v1, v2))) = Map.insert k (f (getVal s v1) (getVal s v2) ) s
-
---getVal' :: CircuitState -> Value -> Number
---getVal' c (Left n) = n
---getVal' c (Right k) = fromJust $ Map.lookup k c
-----getVal' c (Right k) = Map.findWithDefault (fromIntegral 0) k c
-
---getVal' :: Commands -> Value -> Number
---getVal' c (Left n) = n
---getVal' c (Right k) = findWire c k
-
---getVal = memoize2 getVal'
-
-
---findWire' :: Commands -> Command -> Number
---findWire' s (D1 v) =  getVal s v
---findWire' s (D2 (f, v)) =  f (getVal s v)
---findWire' s (D3 (f, v1, v2)) =  f (getVal s v1) (getVal s v2)
-
 findWire :: Commands -> Key -> Number
 findWire s k = findWire' (fromJust $ Map.lookup k s)
     where 
@@ -98,13 +76,13 @@ findWire s k = findWire' (fromJust $ Map.lookup k s)
 
         getVal = memoize getVal'
 
-
---part1 :: [Row] -> CircuitState
---part1 xs =  foldl switch1 (Map.empty)   xs
-
 err a b = error ""
 
 part1 :: [Row] -> Key -> Number
 part1 xs = (findWire (Map.fromListWith  err xs) )
 
-
+part2 :: [Row] -> Key -> Number
+part2 xs k = findWire commands' k
+    where commands = Map.fromListWith  err xs
+          r = part1 xs k
+          commands' = Map.insert "b" (D1 (Left r) ) commands
