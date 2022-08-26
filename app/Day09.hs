8853bd51dd54ee109f6e811f4e31398ec05cfcae
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-08-26 16:21:47 +1200

    Do day 09 part 2

--- app/Day09.hs
+++ app/Day09.hs
@@ -3,19 +3,11 @@ module Day09 where
 import Data.Map (Map)
 import qualified Data.Map as Map
 
---import Data.Set (Set)
---import qualified Data.Set as Set
-
 import Data.List (nub, union, delete, sort)
 
 import Data.Maybe (catMaybes, fromJust, fromMaybe  )
 
 import Data.Function.Memoize
--- doesn't work on dict or set it seems
-
-
-import Debug.Trace
-
 
 
 type Place = String
@@ -45,12 +37,11 @@ requiredCities xs =  sort $   ( nub $ map (!!0) ys   ) `union`  (nub $ map (!!2)
     where ys = map words xs
 
 
-
 travellingSalesman :: Paths -> [Place] -> Maybe Int
-travellingSalesman paths toSee = minimum' $ catMaybes $ traceShowId  $ map (travel toSee) (Map.keys paths )
+travellingSalesman paths toSee = minimum' $ catMaybes   $ map (travel toSee) (Map.keys paths )
     where travel' :: [Place] -> Place -> Maybe Int
           travel' [] _ = Just 0
-          travel' toSee' place = minimum' $ catMaybes $ traceShow (toSee, posses,place,res  ) res
+          travel' toSee' place = minimum' $ catMaybes $  res
             where   toSee = delete  place toSee'
                     posses = filter (\x ->  (elem  (fst x) toSee )  ) (fromMaybe [] $ Map.lookup place paths)
                     res =   [ fmap (cost+) (travel (delete place' toSee ) place')   | (place', cost)  <-  posses  ]
@@ -66,5 +57,7 @@ minimum' xs = Just $ minimum xs
 part1 :: [String] -> Int
 part1 xs = fromJust  $ travellingSalesman (makeGraph xs) (requiredCities xs)
 
+rev (a, b) = (a, (-1)*b)
+
 part2 :: [String] -> Int
-part2 xs = 0
+part2 xs = (-1) * ( fromJust  $ (travellingSalesman ( Map.map (map rev) $ makeGraph xs) ) (requiredCities xs) )
