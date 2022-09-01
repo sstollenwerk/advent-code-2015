67d46a0a2dc147fb75020f6a10fcdf850bace17c
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-09-01 17:49:47 +1200

    Do day12 part 2 in haskell

--- app/Day12.hs
+++ app/Day12.hs
@@ -1,34 +1,48 @@
-
 module Day12 where
-
 
 import Generic (partsSizeN)
 
-
-import Data.List (groupBy,elemIndices, sort)
 import Data.Function (on)
+import Data.List (elemIndex, elemIndices, findIndex, groupBy, sort)
+
+import Data.Maybe (fromJust, fromMaybe, isNothing)
+
+import Debug.Trace (traceShowId)
 
 isNumeric :: Char -> Bool
-isNumeric = flip elem ('-':['0'..'9'])
+isNumeric = flip elem ('-' : ['0' .. '9'])
 
 groupNum :: Char -> Char -> Bool
 groupNum = (==) `on` isNumeric
 
 toNums :: String -> [Int]
-toNums = (map read) . (filter (isNumeric . head  )) . (groupBy groupNum )
+toNums = map read . filter (isNumeric . head) . (groupBy groupNum)
 
 part1 :: String -> Int
 part1 = sum . toNums
 
 toSearch = ":\"red\""
 
+conv :: Char -> Int
+conv '{' = 1
+conv '}' = -1
+conv _ = 0
 
+findMatch :: [Char] -> Int
+findMatch = fromJust . elemIndex (-1) . scanl1 (+) . map conv
+
+removeSection :: String -> Int -> String
+removeSection xs n = take start xs ++ drop end xs
+  where
+    start = last $ elemIndices '{' (take n xs)
+    end = 2 + start + findMatch (drop (start + 1) xs)
+
+findSublist :: Eq a => [a] -> [a] -> Maybe Int
+findSublist part total = elemIndex part (partsSizeN (length part) total)
+
+part2 :: String -> Int
 part2 xs
-    where
-        reds = elemIndices toSearch $ partsSizeN (length red) xs
-        starts = elemIndices '{' xs
-        ends = elemIndices '}' xs
-        parts = sort $ [(i,1) | i <- starts ] ++ [(i,-1) | i <- ends ]
-        (indices, amts) = unzip parts
-        totals = foldl (+) amts
-        places = (map fst) . filter ( (<=2) . snd) $ (zip indices totals)
+  | isNothing red = part1 xs
+  | otherwise = part2 $ removeSection xs (fromJust red)
+  where
+    red = findSublist toSearch xs
--- app/Generic.hs
+++ app/Generic.hs
@@ -1,9 +1,13 @@
 module Generic where
 
+import Data.List (tails, elemIndex)
+
 flist :: [(a -> b)] -> a -> [b]
 flist [] _ = []
 flist (x:xs) n = (x n) : (flist xs n)
 
-
 partsSizeN :: Int -> [a] -> [[a]]
 partsSizeN n = filter ((== n) . length) . map (take n) . tails
+
+findSublist :: Eq a => [a] -> [a] -> Maybe Int
+findSublist part total = elemIndex part (partsSizeN (length part) total)
